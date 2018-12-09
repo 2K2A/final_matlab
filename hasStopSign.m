9b8e04b1a6c3cdbfed21499cc6262240edf15229
Kendel Chopp <kendelchopp@gmail.com>
2018-12-09 13:19:31 -0600

    Whatever (#14)

--- hasStopSign.m
+++ hasStopSign.m
@@ -1,4 +1,4 @@
-function has_stop_sign = hasStopSign(image)
+function [has_stop_sign, confidence_level] = hasStopSign(image)
 score = 0;
 maxScore = 300;
 % Include these in the maxScore
@@ -12,5 +12,5 @@ wordScore = hasWordStop(image);
 % Total score is calculated here
 score = templateScore + featureScore + ratioScore + wordScore;
 
-stop_sign_confidence = score / maxScore;
-has_stop_sign = stop_sign_confidence >= 0.5;
+confidence_level = score / maxScore;
+has_stop_sign = confidence_level >= 0.5;
